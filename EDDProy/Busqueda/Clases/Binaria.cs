a717a8a0ca5e6d7354bd6b4ee994e9c7da71f70a
Angel12rv <al23760339@ite.edu.mx>
2024-12-05 02:41:23 -0800

    Add files via upload

--- EDDProy/Busqueda/Clases/Binaria.cs
+++ EDDProy/Busqueda/Clases/Binaria.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace EDDemo.Busqueda.Clases
 {
-    internal class Binaria
+    class Binaria
     {
+        public List<string> Pasos { get; private set; } = new List<string>();
+
+        public int Buscar(int[] arreglo, int valorBuscado)
+        {
+            Pasos.Clear();
+
+            int inicio = 0;
+            int fin = arreglo.Length - 1;
+
+
+            while (inicio <= fin)
+            {
+                int medio = (inicio + fin) / 2;
+
+                Pasos.Add($"Busquedas en [Derecha pos: {inicio}, Centro pos:{medio}, Izquiera pos:{fin}]");
+
+                if (arreglo[medio] == valorBuscado)
+                {
+                    Pasos.Add($"Valor encontrado en la posición {medio}");
+                    return medio;
+                }
+                else if (arreglo[medio] < valorBuscado)
+                {
+                    inicio = medio + 1;
+                }
+                else
+                {
+                    fin = medio - 1;
+                }
+            }
+
+            Pasos.Add("Valor no encontrado");
+            return -1;
+
+        }
     }
 }
