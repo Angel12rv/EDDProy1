c755cb02a5c188cc34ed9121896c9e90d7c13eb9
angel <angel@LAPTOP-G7ITU7B8>
2024-12-04 21:45:34 -0800

    Agregado al programa busqueda binaria de recursividad anteriormente hecho en cmd

---
+++ EDDProy/Recursividad/Clases/BusquedaBinaria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDDemo.Recursividad.Clases
+{
+    internal class BusquedaBinaria
+    {
+        public static int Buscar(int[] arreglo, int numero, int inicio, int fin, ref int cantidadCortes)
+        {
+            cantidadCortes++;
+
+            if (inicio > fin)
+            {
+                return -1;
+            }
+
+            int medio = (inicio + fin) / 2;
+
+            if (arreglo[medio] == numero)
+            {
+                return medio;
+            }
+
+            if (numero < arreglo[medio])
+            {
+                return Buscar(arreglo, numero, inicio, medio - 1, ref cantidadCortes);
+            }
+
+            return Buscar(arreglo, numero, medio + 1, fin, ref cantidadCortes);
+        }
+    }
+}
